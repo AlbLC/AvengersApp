2f999d73ed529e8ae15b5c6651ca587051145a8b
Alberto Lara <albertolaracaro@gmail.com>
2021-12-05 19:37:05 +0100

    Create Hero Model

---
+++ AvengersApp/Models/Hero.swift
@@ -0,0 +1,14 @@
+//
+//  Hero.swift
+//  AvengersApp
+//
+//  Created by Alberto Lara on 5/12/21.
+//
+
+import Foundation
+
+struct Hero {
+    let image: String
+    let name: String
+    let description: String
+}
