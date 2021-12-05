ce78521f3d27bec1fe30168d009f704cec3dcc68
Alberto Lara <albertolaracaro@gmail.com>
2021-12-05 20:24:15 +0100

    create heroe ViewController

---
+++ AvengersApp/HeroesViewController.swift
@@ -0,0 +1,32 @@
+//
+//  HeroesViewController.swift
+//  AvengersApp
+//
+//  Created by Alberto Lara on 5/12/21.
+//
+
+import UIKit
+
+class HeroesViewController: UIViewController {
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
