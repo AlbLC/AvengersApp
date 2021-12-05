cad21e98fb6ab682d8b498248be3e8730f070e88
Alberto Lara <albertolaracaro@gmail.com>
2021-12-05 19:54:56 +0100

    create heroes repository

---
+++ AvengersApp/Repositories/HeroesRepository.swift
@@ -0,0 +1,43 @@
+//
+//  HeroesRepository.swift
+//  AvengersApp
+//
+//  Created by Alberto Lara on 5/12/21.
+//
+
+import Foundation
+
+
+class HeroRepository {
+    
+    let heroes = [Hero(image: "img_heroe_america_captain",
+                       name: "Capitan America",
+                       description: "Descripcion capitan america"),
+                  Hero(image: "img_heroe_black_panther",
+                                     name: "Pantera negra",
+                                     description: "Descripcion pantera negra"),
+                  Hero(image: "img_heroe_america_captain",
+                                     name: "Capitan America",
+                                     description: "Descripcion capitan america"),
+                  Hero(image: "img_heroe_america_captain",
+                                     name: "Capitan America",
+                                     description: "Descripcion capitan america"),
+                  Hero(image: "img_heroe_america_captain",
+                                     name: "Capitan America",
+                                     description: "Descripcion capitan america"),
+                  Hero(image: "img_heroe_america_captain",
+                                     name: "Capitan America",
+                                     description: "Descripcion capitan america"),
+                  Hero(image: "img_heroe_america_captain",
+                                     name: "Capitan America",
+                                     description: "Descripcion capitan america"),
+                  Hero(image: "img_heroe_america_captain",
+                                     name: "Capitan America",
+                                     description: "Descripcion capitan america"),
+                  Hero(image: "img_heroe_america_captain",
+                                     name: "Capitan America",
+                                     description: "Descripcion capitan america"),
+                  Hero(image: "img_heroe_america_captain",
+                                     name: "Capitan America",
+                                     description: "Descripcion capitan america")]
+}
